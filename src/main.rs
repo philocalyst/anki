65e7088d685b950d2f87017788c893d390a323b8
miles <regime_skylark.0y@icloud.com>
2025-07-31 13:50:38 -0400

    Got a working rust example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let example_content = include_str!("/home/miles/Downloads/oh/example.flash");
+
+    let mut current_note_model = "".to_string();
+
+    let mut question_field = "".to_string();
+    let mut answer_field = "".to_string();
+
+    for line in example_content.lines() {
+
+        if line.contains("=") {
+            current_note_model = line.replace("=", "");
+        } else if line.starts_with("Question:") {
+            question_field = line.replace("Question:", "");
+        } else if line.starts_with("Answer:") {
+            answer_field = line.replace("Answer:", "");
+        } else {
+            continue;
+        }
+        
+        println!("{}{}{}", current_note_model, question_field, answer_field);
+    }
 }
