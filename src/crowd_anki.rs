7af89e395b6ac75cab7f908855401e9fc855dd5d
miles <regime_skylark.0y@icloud.com>
2025-08-23 15:21:14 -0400

    Brought up to full spec

--- src/crowd_anki.rs
+++ src/crowd_anki.rs
@@ -6,7 +6,7 @@ pub enum NoteModelType {
     Cloze,
 }
 
-impl<'de> serde::Deserialize<'de> for NoteModelType {
+impl<'de> Deserialize<'de> for NoteModelType {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
@@ -20,7 +20,7 @@ impl<'de> serde::Deserialize<'de> for NoteModelType {
     }
 }
 
-impl serde::Serialize for NoteModelType {
+impl Serialize for NoteModelType {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
@@ -49,7 +49,20 @@ pub struct Deck {
     pub name: String,
     pub id: i64,
     pub uuid: String,
+
+    pub crowdanki_uuid: String,
+
     pub deck_config_uuid: String,
+    pub desc: String,
+
+    #[serde(rename = "dyn")]
+    pub is_dynamic: i32,
+
+    #[serde(rename = "extendNew")]
+    pub extend_new: i32,
+
+    #[serde(rename = "extendRev")]
+    pub extend_rev: i32,
 
     pub note_models: Vec<NoteModel>,
     pub deck_configurations: Vec<DeckConfig>,
@@ -63,12 +76,30 @@ pub struct Deck {
 pub struct NoteModel {
     pub id: i64,
     pub uuid: String,
+
+    #[serde(rename = "crowdanki_uuid")]
+    pub crowdanki_uuid: String,
+
     pub name: String,
-    pub r#type: NoteModelType, // 0 = standard, 1 = cloze
+
+    #[serde(rename = "type")]
+    pub kind: NoteModelType,
+
     pub flds: Vec<Field>,
     pub tmpls: Vec<Template>,
     pub css: String,
-    pub did: Option<String>, // deck uuid
+    pub did: Option<String>,
+
+    #[serde(rename = "latexPre")]
+    pub latex_pre: Option<String>,
+
+    #[serde(rename = "latexPost")]
+    pub latex_post: Option<String>,
+
+    pub req: Option<Vec<(i32, String, Vec<i32>)>>,
+    pub sortf: Option<i32>,
+    pub tags: Option<Vec<String>>,
+    pub vers: Option<Vec<String>>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -80,6 +111,7 @@ pub struct Field {
     pub rtl: bool,
     pub font: String,
     pub size: i32,
+    pub media: Vec<String>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -91,30 +123,52 @@ pub struct Template {
     pub afmt: String,
     pub bafmt: Option<String>,
     pub bqfmt: Option<String>,
+    pub did: Option<String>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone)]
 pub struct Note {
     pub guid: String,
-    pub uuid: String,
+    pub uuid: Option<String>,
+
+    #[serde(rename = "note_model_uuid")]
     pub note_model_uuid: String,
+
     pub fields: Vec<String>,
     pub tags: Vec<String>,
     pub flags: i32,
+
+    #[serde(default)]
+    pub newly_added: bool,
+
+    #[serde(default)]
+    pub data: Option<String>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone)]
 pub struct DeckConfig {
     pub uuid: String,
+
+    #[serde(rename = "crowdanki_uuid")]
+    pub crowdanki_uuid: String,
+
     pub name: String,
     pub id: i64,
 
-    pub maxTaken: Option<i32>,
+    #[serde(rename = "dyn")]
+    pub is_dynamic: bool,
+
+    pub max_taken: Option<i32>,
     pub new: Option<NewConfig>,
     pub rev: Option<RevConfig>,
     pub lapse: Option<LapseConfig>,
+
+    pub autoplay: Option<bool>,
+    pub replayq: Option<bool>,
+    pub timer: Option<i32>,
+    pub another_retreat: Option<bool>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -122,16 +176,24 @@ pub struct DeckConfig {
 pub struct NewConfig {
     pub delays: Vec<i32>,
     pub ints: Vec<i32>,
-    pub initialFactor: i32,
-    pub perDay: i32,
+    pub initial_factor: i32,
+    pub per_day: i32,
+    pub order: Option<i32>,
+    pub bury: Option<bool>,
+    pub separate: Option<bool>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone)]
 pub struct RevConfig {
-    pub perDay: i32,
+    pub per_day: i32,
     pub ease4: f32,
-    pub ivlFct: f32,
+    pub ivl_fct: f32,
+    pub fuzz: Option<f32>,
+    pub hard_factor: Option<f32>,
+    pub max_ivl: Option<i32>,
+    pub min_space: Option<i32>,
+    pub bury: Option<bool>,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -139,5 +201,7 @@ pub struct RevConfig {
 pub struct LapseConfig {
     pub delays: Vec<i32>,
     pub mult: f32,
-    pub minInt: i32,
+    pub min_int: i32,
+    pub leech_action: Option<i32>,
+    pub leech_fails: Option<i32>,
 }
