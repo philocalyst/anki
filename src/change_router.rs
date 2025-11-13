7e480a82ea27defbe9582dcbf033336fd2970c9e
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-12 22:47:30 -0500

    Created a rough prototype for the change router function
    Should effectively calculate two of the three kinds of changes

--- src/change_router.rs
+++ src/change_router.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use crate::types::deck::Deck;
+use crate::types::{deck::Deck, note::Note};
 
 pub(crate) enum ChangeType {
 	Addition,
@@ -11,8 +11,41 @@ pub(crate) enum ChangeType {
 /// Determines the kind of change occured between two decks. A None value is
 /// return when no change has occured.
 pub(crate) fn determine_change(
-	deck_1: &Deck,
-	deck_2: &Deck,
+	deck_1: &Vec<Note>,
+	deck_2: &Vec<Note>,
 ) -> Result<Option<ChangeType>, Box<dyn Error>> {
+	if deck_1 == deck_2 {
+		return Ok(None);
+	}
+
+	// This is likely to be a deletion or modification, the job now is to determine
+	// which one and where
+	if deck_1.len() != deck_2.len() {
+		for ((index1, card1), (index2, card2)) in
+			deck_1.into_iter().enumerate().zip(deck_2.into_iter().enumerate())
+		{
+			// We've identified a divergence.
+			if card1 != card2 {
+				// We're checking if this card is now out of order or no longer exists
+				if exists_in_deck(deck_2, card1) {
+					// If it does exist, it means that there was an addition
+					return Ok(Some(ChangeType::Addition));
+				} else {
+					// This is the case where it no longer exists
+					return Ok(Some(ChangeType::Deletion));
+				}
+			}
+		}
+	}
+
 	Ok(None)
+}
+
+fn exists_in_deck(deck: &Vec<Note>, target: &Note) -> bool {
+	for note in deck {
+		if note == target {
+			return true;
+		}
+	}
+	return false;
 }
