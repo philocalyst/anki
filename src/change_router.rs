748e098ca355b8438437a21e3da5fa87495e8a9d
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-16 10:02:53 -0500

    Modified to return a list of changes
    Should allow a more fluid and dynamic editing process

--- src/change_router.rs
+++ src/change_router.rs
@@ -12,59 +12,89 @@ pub enum ChangeType<'a> {
 
 /// Determines the kind of change occured between two decks. A None value is
 /// return when no change has occured.
-pub fn determine_change<'a>(
+pub fn determine_changes<'a>(
 	deck_1: &'a Vec<Note<'a>>,
 	deck_2: &'a Vec<Note<'a>>,
-) -> Result<Option<ChangeType<'a>>, Box<dyn Error>> {
+) -> Result<Vec<ChangeType<'a>>, Box<dyn Error>> {
+	// Early return if decks are identical - no changes needed
 	if deck_1 == deck_2 {
-		return Ok(None);
+		return Ok(vec![]);
 	}
 
+	// Case 1: Different lengths - either all additions or all deletions
+	// We can't mix these types because indices would become inconsistent
 	if deck_1.len() != deck_2.len() {
+		if deck_2.len() > deck_1.len() {
+			// Deck grew - find all additions by walking both decks
+			let mut additions = Vec::new();
+			let mut deck_1_idx = 0;
+			let mut deck_2_idx = 0;
+
+			while deck_2_idx < deck_2.len() {
+				if deck_1_idx < deck_1.len() && deck_1[deck_1_idx] == deck_2[deck_2_idx] {
+					// Cards match, advance both pointers
+					deck_1_idx += 1;
+					deck_2_idx += 1;
+				} else {
+					// Card at deck_2_idx is new - record the addition
+					additions.push(ChangeType::Addition((deck_2_idx, &deck_2[deck_2_idx])));
+					deck_2_idx += 1;
+				}
+			}
+			return Ok(additions);
+		} else {
+			// Deck shrank - find all deletions by walking both decks
+			let mut deletions = Vec::new();
+			let mut deck_1_idx = 0;
+			let mut deck_2_idx = 0;
+
+			while deck_1_idx < deck_1.len() {
+				if deck_2_idx < deck_2.len() && deck_1[deck_1_idx] == deck_2[deck_2_idx] {
+					// Cards match, advance both pointers
+					deck_1_idx += 1;
+					deck_2_idx += 1;
+				} else {
+					// Card at deck_1_idx was deleted - record the deletion
+					deletions.push(ChangeType::Deletion(deck_1_idx));
+					deck_1_idx += 1;
+				}
+			}
+			// IMPORTANT: Deletions must be applied in reverse order to maintain
+			// index consistency. When you delete at index 0, everything shifts down,
+			// so we need to delete from the end first.
+			deletions.reverse();
+			return Ok(deletions);
+		}
+	}
+
+	// Case 2: Same length - could be reordering or modifications
+	// Check if it's a reorder by comparing sorted versions
+	let mut sorted_1 = deck_1.clone();
+	let mut sorted_2 = deck_2.clone();
+	sorted_1.sort();
+	sorted_2.sort();
+
+	if sorted_1 == sorted_2 {
+		// Same cards, different order - this is a reordering
+		// Find all positions where cards differ
+		let mut reorderings = Vec::new();
 		for (index, (card1, card2)) in deck_1.iter().zip(deck_2.iter()).enumerate() {
 			if card1 != card2 {
-				// We're checking if this card is now out of order or no longer exists
-				if deck_2.iter().find(|n| *n == card1).is_some() {
-					// It does exist, this means that there was an addition
-					return Ok(Some(ChangeType::Addition((index, &card2))));
-				} else {
-					// This is the case where it no longer exists
-					return Ok(Some(ChangeType::Deletion(index)));
-				}
+				// TODO: This index alone isn't enough info for reordering.
+				// We need to track where each card moved from/to.
+				reorderings.push(ChangeType::Reordering(index));
 			}
 		}
-
-		// Handle trailing additions/deletions
-		if deck_1.len() < deck_2.len() {
-			return Ok(Some(ChangeType::Addition((deck_1.len(), &deck_2.last().unwrap()))));
-		} else {
-			return Ok(Some(ChangeType::Deletion(deck_2.len())));
-		}
+		return Ok(reorderings);
 	} else {
-		// Same length - could be modification or reordering
-		// Check if it's a reorder (same cards, different order)
-		let mut sorted_1 = deck_1.clone();
-		let mut sorted_2 = deck_2.clone();
-		sorted_1.sort();
-		sorted_2.sort();
-
-		if sorted_1 == sorted_2 {
-			// It's a reorder - find first difference
-			for (index, (card1, card2)) in deck_1.iter().zip(deck_2.iter()).enumerate() {
-				if card1 != card2 {
-					// TODO: Fix this to return a precise mapping of old to new, index isn't enough.
-					return Ok(Some(ChangeType::Reordering(index)));
-				}
-			}
-		} else {
-			// It's a modification - find first difference among the cards
-			for (index, (card1, card2)) in deck_1.iter().zip(deck_2.iter()).enumerate() {
-				if card1 != card2 {
-					return Ok(Some(ChangeType::Modification((index, card2))));
-				}
+		// Different cards at same positions - these are modifications
+		// Find all positions where content changed
+		let mut modifications = Vec::new();
+		for (index, (card1, card2)) in deck_1.iter().zip(deck_2.iter()).enumerate() {
+			if card1 != card2 {
+				modifications.push(ChangeType::Modification((index, card2)));
 			}
 		}
+		return Ok(modifications);
 	}
-
-	Ok(None)
 }
